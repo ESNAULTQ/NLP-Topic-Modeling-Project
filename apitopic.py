52ae71fdf95d71bd2c0f730550ad60c64ead5e50
Mohamed Benalia <benaliamohamedpro@gmail.com>
2024-09-04 12:00:12 +0200

    nettoyage et structuration

--- apitopic.py
+++ apitopic.py
@@ -1,19 +1,26 @@
 # api.py
-from fastapi import FastAPI
-import numpy as np
+from fastapi import FastAPI, HTTPException
 import joblib
-from model import lda_model
-
+from pydantic import BaseModel
 # Charger le modèle LDA et le vectorizer sauvegardés
-lda_model = joblib.load('lda_model.pkl')
-vectorizer = joblib.load('vectorizer.pkl')
+lda_model = joblib.load('models/model_1/lda_model.pkl')
+vectorizer = joblib.load('models/model_1/vectorizer.pkl')
 
 # Initialiser FastAPI
 app = FastAPI()
 
+class TextInput(BaseModel):
+    text: str
+
 # Route pour faire une prédiction avec une requête GET
-@app.get("/predict")
-def predict(text: str):
+@app.post("/predict")
+def predict(input_data: TextInput):
+    text = input_data.text
+
+    # Vérifier si le texte est vide
+    if not text.strip():
+        raise HTTPException(status_code=400, detail="Le texte fourni est vide.")
+
     # Transformer le texte en vecteur à l'aide du vectorizer
     text_vector = vectorizer.transform([text])
 
@@ -22,3 +29,6 @@ def predict(text: str):
 
     # Retourner la distribution des topics
     return {"topic_distribution": topic_distribution.tolist()}
+@app.get("/status")
+def status():
+    return {'message':'API en ligne!'}
